950467c69b68f51eefa1bf02e5198e36f374035a
chen-z <mythc21@yahoo.com>
2014-08-10 23:01:27 +0800

    update for homework

--- plot4.R
+++ plot4.R
@@ -1,3 +1,4 @@
+# data science project1
 if (!file.exists("data.zip")) {
   download.file(url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
                 destfile="data.zip",
